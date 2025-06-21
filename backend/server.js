a3e0d6b0cde7c5f8a01034be6f0848a18f67589e
ujjwal john <ujjwaljohn395@gmail.com>
2025-06-22 01:22:52 +0530

    next fixec0

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const taskRoutes = require('./routes/taskroutes.js');
+const taskRoutes = require('./routes/taskroutes');
 const dotenv = require("dotenv").config()
 const Port = process.env.PORT || 5000;
 
